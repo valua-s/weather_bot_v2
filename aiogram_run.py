4eb7a4de7c7f0a26ae82d83ea00ff2361eed0266
Valua-s <speranskaya.valya@yandex.ru>
2024-09-10 11:12:12 +0800

    adding keyboards

--- aiogram_run.py
+++ aiogram_run.py
@@ -2,8 +2,16 @@ import asyncio
 
 from create_bot import bot, dp  # , scheduler
 from handlers.start import start_router
-from handlers.menu_commands import set_commands
 # from work_time.time_func import send_time_msg
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+
+async def set_commands(bot):
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='forcast', description='Составить прогноз'),
+                BotCommand(command='cancel', description='Отмена подписки')]
+                # BotCommand(command='start_3', description='Старт 3')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
 
 
 async def main():
@@ -12,7 +20,7 @@ async def main():
     dp.include_router(start_router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-    await set_commands(bot)
+    await set_commands()
 
 if __name__ == "__main__":
     asyncio.run(main())
--- handlers/menu_commands.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import BotCommand, BotCommandScopeDefault
-
-
-async def set_commands(bot):
-    commands = [BotCommand(command='start', description='Старт'),
-                BotCommand(command='forcast', description='Составить прогноз'),
-                BotCommand(command='cancel', description='Отмена подписки')]
-                # BotCommand(command='start_3', description='Старт 3')]
-    await bot.set_my_commands(commands, BotCommandScopeDefault())
